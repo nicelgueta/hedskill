5601bf2fac0b17f559f59aa75019087fb4b6b9e8
nicelgueta <nicelgueta@gmail.com>
2022-01-09 19:38:56 +0000

    resolved expo env issues

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { NativeBaseProvider, Box } from 'native-base';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box style={styles.container}>
+        <Text>Great - now we're off</Text>
+        <StatusBar style="auto" />
+      </Box>
+    </NativeBaseProvider>
   );
 }
 
